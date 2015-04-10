a460c3a5d65c6b201cf1e6875e58b23f851783ad
Aaron Crespo <aaroncrespo@users.noreply.github.com>
2015-04-10 09:37:31 -0400

    Silence as build warning, for size mismatch
    `deadline` is an NSTimerInterval which is a typedef on double. Therefore `fabs` should be used.

--- DiffMatchPatch/DiffMatchPatch.m
+++ DiffMatchPatch/DiffMatchPatch.m
@@ -121,7 +121,7 @@ NSMutableArray *diff_diffsBetweenTextsWithProperties(NSString *text1, NSString *
 	
 	
 	// Test if the deadline is zero or has already passed
-	if(fabsf(properties.deadline) < 0.00000001) {
+	if(fabs(properties.deadline) < 0.00000001) {
 		properties.deadline = [[NSDate distantFuture] timeIntervalSinceReferenceDate];
 	} else {
 		NSTimeInterval currentTime = [NSDate timeIntervalSinceReferenceDate];
