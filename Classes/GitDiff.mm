d8bafe6cd28bf3506ff68033be48d649e68aba7e
John Holdsworth <github@johnholdsworth.com>
2017-05-10 16:01:25 +0100

    Xcode 8.3

--- Classes/GitDiff.mm
+++ Classes/GitDiff.mm
@@ -4,7 +4,7 @@
 //
 //  Repo: https://github.com/johnno1962/GitDiff
 //
-//  $Id: //depot/GitDiff/Classes/GitDiff.mm#84 $
+//  $Id: //depot/GitDiff/Classes/GitDiff.mm#85 $
 //
 //  Created by John Holdsworth on 26/07/2014.
 //  Copyright (c) 2014 John Holdsworth. All rights reserved.
